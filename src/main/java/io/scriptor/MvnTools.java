cdbb70d69ec9f322b3bd8b5fc7efb977fe2c8922
Felix Schreiber <felix.schreiber@auctores.de>
2024-07-09 13:11:52 +0200

    refractoring

--- src/main/java/io/scriptor/MvnTools.java
+++ src/main/java/io/scriptor/MvnTools.java
@@ -8,16 +8,7 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-
 public class MvnTools {
-
-    public static void main(String[] args) throws IOException {
-        final var root = MvnArtifact.getArtifact("io.scriptor:mvntools:1.0.0");
-        root.dumpTree();
-        Graphviz.fromGraph(root.generateGraph()).height(1000).render(Format.PNG).toFile(new File("export.png"));
-    }
 
     public static File getRepository() throws IOException {
         final var home = System.getProperty("user.home");

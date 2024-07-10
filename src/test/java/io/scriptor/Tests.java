9b5db8e44757a49d07214eefb6a00e3071c81231
Felix Schreiber <felix.schreiber@auctores.de>
2024-07-10 11:29:29 +0200

    gave tests display names

--- src/test/java/io/scriptor/Tests.java
+++ src/test/java/io/scriptor/Tests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import guru.nidi.graphviz.engine.Format;
@@ -15,18 +16,21 @@ public class Tests {
     public static final String ID = "org.apache.maven:maven-core:3.9.8";
 
     @Test
+    @DisplayName("Simple Artifact Get")
     public void testGetArtifact() throws IOException {
         final var artifact = MvnArtifact.getArtifact(ID);
         assertNotNull(artifact);
     }
 
     @Test
+    @DisplayName("Simple Artifact Get + Dump Dependency Tree")
     public void testDumpTree() throws IOException {
         final var root = MvnArtifact.getArtifact(ID);
         root.dumpTree();
     }
 
     @Test
+    @DisplayName("Simple Artifact Get + Export GraphViz Graph")
     public void testExportGraph() throws IOException {
         final var root = MvnArtifact.getArtifact(ID);
         Graphviz.fromGraph(root.generateGraph())
